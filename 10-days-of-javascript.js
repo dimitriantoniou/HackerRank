21f2002afca5afddeee5b64b1064550937c5bad8
dimitriantoniou <dma2012@gmail.com>
2020-08-20 08:21:05 -0500

    10 days of js day 4

--- 10-days-of-javascript.js
+++ 10-days-of-javascript.js
@@ -164,3 +164,50 @@ function isPositive(a) {
         throw Error ("Negative Error");
     }
 }
+
+//day 4
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    this.length = a;
+    this.width = b;
+    this.area = a * b;
+    this.perimeter = 2 * (a + b);
+}
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ *
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    let count=0;
+    for (var i=0;i<objects.length;i++){
+        if (objects[i].x == objects[i].y){
+            count ++;
+        }
+    }
+    return count;
+}
+
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+
+
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+    perimeter() {
+        var sum = 0;
+        for(var i=0;i< this.sides.length; i++){
+            sum += + this.sides[i];
+        }
+        return sum;
+    }
+}
